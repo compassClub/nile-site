de348b799258660b5a005f5ca7ecfff0d632ea37
compassClub <compassClub@users.noreply.github.com>
2017-06-10 12:13:08 -0500

    Reinstalled the Project

--- nileAngularSite/src/environments/environment.prod.ts
+++ nileAngularSite/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDTIurExNeiE6sCygDP5YufICfu2dTYtFU",
+    authDomain: "nile-dc973.firebaseapp.com",
+    databaseURL: "https://nile-dc973.firebaseio.com",
+    projectId: "nile-dc973",
+    storageBucket: "nile-dc973.appspot.com",
+    messagingSenderId: "879987418484"
+  }
 };
--- nileAngularSite/src/environments/environment.ts
+++ nileAngularSite/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDTIurExNeiE6sCygDP5YufICfu2dTYtFU",
+    authDomain: "nile-dc973.firebaseapp.com",
+    databaseURL: "https://nile-dc973.firebaseio.com",
+    projectId: "nile-dc973",
+    storageBucket: "nile-dc973.appspot.com",
+    messagingSenderId: "879987418484"
+  }
 };
